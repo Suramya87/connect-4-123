fd24dbeca2ce283a648bc502419424311dfca11b
Suramya <146380955+Suramya87@users.noreply.github.com>
2026-02-03 20:41:45 -0800

    adding ignore file

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/shaky/OneDrive/Desktop/CMPM123/Cmpm123/connect-4-123
-# Build directory: C:/Users/shaky/OneDrive/Desktop/CMPM123/Cmpm123/connect-4-123/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
